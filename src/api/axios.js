f294da5d37cd847b08bd209db0216e2ef328f546
H-Dohyun <dohyun033@gmail.com>
2022-09-14 20:33:18 +0900

    env 적용
    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "524ac9818852cda2f05f8ae4e7507f48",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
