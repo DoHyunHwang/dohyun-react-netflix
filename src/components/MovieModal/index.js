fcc2a2a5c6644957cc0502354caa9fe10f1b4dce
H-Dohyun <dohyun033@gmail.com>
2022-09-14 20:24:00 +0900

    Swiper 라이브러리 적용
    .

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef } from "react";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 import "./MovieModal.scss";
 
